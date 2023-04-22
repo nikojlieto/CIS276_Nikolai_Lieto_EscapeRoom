75129edede0b5266bb71a2b3157eb578a7649799
nikojlieto <nikojlieto@gmail.com>
2023-04-21 21:30:55 -0400

    updatedand switch buttons

---
+++ CIS276_Nikolai_Lieto_EscapeRoom/Assets/CenterSceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CenterSceneLoader : MonoBehaviour
+{
+public const string GAME_SCENE = "CenterScene";
+    [SerializeField]
+    private Button loadSceneButton;
+    private void Start()
+    {
+        loadSceneButton.onClick.AddListener(LoadScene);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(GAME_SCENE);
+    }
+}
---
+++ CIS276_Nikolai_Lieto_EscapeRoom/Assets/RealCenterSceneLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RealCenterSceneLoader : MonoBehaviour
+{
+public const string LEFT_SCENE = "LeftScene";
+public const string RIGHT_SCENE = "RightScene";
+    [SerializeField]
+    private Button loadLeftSceneButton;
+    [SerializeField]
+    private Button loadRightSceneButton;
+    private void Start()
+    {
+        loadLeftSceneButton.onClick.AddListener(LoadLeftScene);
+        loadRightSceneButton.onClick.AddListener(LoadRightScene);
+    }
+
+    public void LoadLeftScene()
+    {
+        SceneManager.LoadScene(LEFT_SCENE);
+    }
+
+    public void LoadRightScene()
+    {
+        SceneManager.LoadScene(RIGHT_SCENE);
+    }
+}
+
